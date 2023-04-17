cb92b05e03adfd7de2eabf423c86fceac2377bfd
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-04-17 14:32:14 +0200

    Create decompress_blob.js

---
+++ decompress_blob.js
@@ -0,0 +1,14 @@
+/**
+ * This function decompresses a compressed blob object. This function may not work under Firefox. Check browser
+ * compatibility for more information.
+ * 
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Compression_Streams_API#browser_compatibility)
+ * @param {Blob} compressedBlob Compressed blob object
+ * @param {string} format Decompression format
+ * @returns {Blob} Decompressed blob object
+ */
+export function decompressBlob(compressedBlob = new Blob(), format = 'gzip') {
+    const ds = new DecompressionStream(format);
+    const decompressedStream = compressedBlob.stream().pipeThrough(ds);
+    return new Response(decompressedStream).blob();
+}
